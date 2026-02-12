2c066012d0dbdcb14a0ec9ead52950e9df065d96
khairellah <zakariakhairellah@gmail.com>
2026-02-12 22:43:04 +0100

    KC-13/US-0.5 Création automatique du compte Admin par défaut - Implémentation héritage BaseEntity -> AppUser -> Admin/Client/Company et DataInitializer

---
+++ Backend/src/main/java/com/kriliCar/entities/Company.java
@@ -0,0 +1,15 @@
+package com.kriliCar.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "companies")
+// @PrimaryKeyJoinColumn(name = "company_id")
+@Getter @Setter @NoArgsConstructor
+@SuperBuilder
+public class Company extends AppUser {
+}
---
+++ Backend/src/main/java/com/kriliCar/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kriliCar.repositories;
+
+
+import com.kriliCar.entities.AppUser;
+import com.kriliCar.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
